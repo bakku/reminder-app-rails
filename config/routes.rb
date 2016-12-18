231362b3054437b517f12bcbe218a7d06e73e797
Christian Paling <christian.paling@googlemail.com>
2016-12-18 13:19:34 +0100

    Remove coffee script from Gemfile and add reminders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'reminders_controller/index'
+
+  get 'reminders_controller/show'
+
+  get 'reminders_controller/create'
+
+  get 'reminders_controller/update'
+
+  get 'reminders_controller/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
