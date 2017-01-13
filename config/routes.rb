f585e34343a708e77a7951630e3228c8193d8f96
Christian Paling <christian.paling@googlemail.com>
2017-01-13 21:06:31 +0100

    Add routes for reminders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'reminders',    to: 'reminders#index'
-
   # users
   get    'users',       to: 'users#index'
   post   'users',       to: 'users#create'
   get    'users/:id',   to: 'users#show', as: :user_show
   delete 'users/:id',   to: 'users#delete'
+
+  # reminders
+  get    '/users/:id/reminders',               to: 'reminders#index'
+  post   '/users/:id/reminders',               to: 'reminders#create'
+  get    '/users/:id/reminders/:reminder_id',  to: 'reminders#show'
+  delete '/users/:id/reminders/:reminder_id',  to: 'reminders#delete'
+  
 end
